04fd7f0197ae658e6384ebe59dfc1e74f27142ea
Lighthit <poomipatsubinmit@gmail.com>
2025-04-27 14:26:40 +0700

    add env for handle IP & Port

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import dotenv from 'dotenv'
+import react from '@vitejs/plugin-react'  // Import react plugin ที่ถูกต้อง
+dotenv.config();
 // https://vite.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: process.env.HOST || "127.0.0.1",
+    port: process.env.PORT_WEB || 5173,
     proxy: {
-      '/api': 'http://0.0.0.0:55543',
+      '/api': `http://${process.env.HOST || "127.0.0.1" }:${process.env.PORT_SERVER || 11111}`,
     },
   }
 })
